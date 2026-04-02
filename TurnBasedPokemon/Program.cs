75f4f507bd30ae3ceb4fd6398a9caf9fb6531b82
unknown <dangnhabui@gmail.com>
2026-04-02 17:25:54 +0700

    update

--- TurnBasedPokemon/Program.cs
+++ TurnBasedPokemon/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            // Init Repo -> get Data first
             UserRepository userRepo = new UserRepository();
             PokedexRepository pokedexRepo = new PokedexRepository();
             pokedexRepo.LoadData();
@@ -11,10 +12,10 @@
             itemRepository.LoadData();
 
             GameSession gameSession = new GameSession();
-
             PokemonSpawner pokemonSpawner = new PokemonSpawner(pokedexRepo);
             ItemSpawner itemSpawner = new ItemSpawner(itemRepository);
 
+            // Init Services
             ScreenManager screenManager = new ScreenManager();
             WorldGenerationService worldGenerationService = new WorldGenerationService(gameSession, pokemonSpawner, itemSpawner);
             AuthenService authenService = new AuthenService(userRepo, gameSession);
@@ -23,12 +24,14 @@
             CatchService catchService = new CatchService(gameSession);
             HealingService healingService = new HealingService(gameSession);
 
+            // init Application
             screenManager.RegisterScreen(ScreenType.Login, new LoginScreen(screenManager, gameSession, authenService));
             screenManager.RegisterScreen(ScreenType.MainMenu, new MainMenuScreen(screenManager, gameSession));
             screenManager.RegisterScreen(ScreenType.Story, new StoryScreen(screenManager, gameSession, storyService));
             screenManager.RegisterScreen(ScreenType.Battle, new BattleScreen(screenManager, gameSession, battleService));
             screenManager.RegisterScreen(ScreenType.Inventory, new InventoryScreen(screenManager, gameSession, catchService, healingService));
 
+            // Run Application
             screenManager.Run();
         }
     }
